e366e06c5af46297e564dfd0a56042a78d46d9c0
Ark444 <quentrg@gmail.com>
2016-06-24 13:54:47 +0200

    add error handling

--- jishearch.py
+++ jishearch.py
@@ -19,16 +19,20 @@ url = 'http://beta.jisho.org/api/v1/search/words?keyword='
 
 def jisho_search(data, buffer, message):
     r = requests.get(url+message)
-    data = json.loads(r.text)['data'][0]
-
-    result = b''
+    
+    result = b'[JiSearch] '
     try:
-        result += b'kanji: %s | ' % data['japanese'][0]['word'].encode('utf-8')
-    except KeyError:
-        pass
-    result += b'reading: %s | ' % data['japanese'][0]['reading'].encode('utf-8')
-    result += b'meaning: %s' % data['senses'][0]['english_definitions'][0].encode('utf-8')
+        data = json.loads(r.text)['data'][0]
 
+        try:
+            result += b'kanji: %s | ' % data['japanese'][0]['word'].encode('utf-8')
+        except KeyError:
+            pass
+        result += b'reading: %s | ' % data['japanese'][0]['reading'].encode('utf-8')
+        result += b'meaning: %s' % data['senses'][0]['english_definitions'][0].encode('utf-8')
+
+    except:
+        result += 'no results found.'
     weechat.prnt(weechat.current_buffer(), result)
     return weechat.WEECHAT_RC_OK
 
